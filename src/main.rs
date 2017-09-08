d0a1e5eb07667a3a2f2c33b9acd1119f205ec7ee
Igaguri <igagurimk@gmail.com>
2017-09-08 15:58:42 +0900

    文字列の入力を追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+use std::error;
+use std::fmt;
+use std::io;
+use std::io::Write;
+use std::result;
+
 fn main() {
-    println!("Hello, world!");
+    match calc_expr() {
+        Ok(()) => (),
+        Err(err) => println!("Error: {}", err),
+    }
+}
+
+fn calc_expr() -> Result<()> {
+    print!("Input expr:");
+    io::stdout().flush()?;
+
+    let line = read_expr()?;
+
+    println!("bytes:{}", line);
+
+    Ok(())
+}
+
+fn read_expr() -> Result<String> {
+    let stdin = io::stdin();
+    
+    let mut line = String::new();
+    stdin.read_line(&mut line)?;
+
+    Ok(line)
+}
+
+
+#[derive(Debug)]
+enum CalcError {
+    Io(io::Error),
+}
+
+type Result<T> = result::Result<T, CalcError>;
+
+impl fmt::Display for CalcError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            CalcError::Io(ref err) => write!(f, "IO error:{}", err),
+        }
+    }
+}
+
+impl error::Error for CalcError {
+    fn description(&self) -> &str {
+        match *self {
+            CalcError::Io(ref err) => err.description(),
+        }
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        match *self {
+            CalcError::Io(ref err) => Some(err),
+        }
+    }
+}
+
+impl From<io::Error> for CalcError {
+    fn from(err: io::Error) -> CalcError {
+        CalcError::Io(err)
+    }
 }
