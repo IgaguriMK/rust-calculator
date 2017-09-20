71d5719c1007d90166724ce22110164f674f262b
Igaguri <igagurimk@gmail.com>
2017-09-20 14:53:37 +0900

    executeの処理を記述

--- src/execute/error.rs
+++ src/execute/error.rs
@@ -4,20 +4,20 @@ use std::result;
 
 #[derive(Debug)]
 pub enum ExecuteError {
-    ZeroDiv(String)
+    OutOfDef(String)
 }
 
 impl ExecuteError {
-    pub fn zero_div<S>(message: S) -> ExecuteError
+    pub fn out_of_def<S>(message: S) -> ExecuteError
     where
         S: Into<String>,
     {
-        ExecuteError::ZeroDiv(message.into())
+        ExecuteError::OutOfDef(message.into())
     }
 
     pub fn get_message(&self) -> &str {
         match *self {
-            ExecuteError::ZeroDiv(ref message) => message,
+            ExecuteError::OutOfDef(ref message) => message,
         }
     }
 }
@@ -27,7 +27,7 @@ pub type ExecuteResult<T> = result::Result<T, ExecuteError>;
 impl fmt::Display for ExecuteError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            ExecuteError::ZeroDiv(ref message) => write!(f, "{}", message),
+            ExecuteError::OutOfDef(ref message) => write!(f, "{}", message),
         }
     }
 }
@@ -36,7 +36,7 @@ impl fmt::Display for ExecuteError {
 impl error::Error for ExecuteError {
     fn description(&self) -> &str {
         match *self {
-            ExecuteError::ZeroDiv(_) => "Paren mismatch.",
+            ExecuteError::OutOfDef(_) => "Paren mismatch.",
         }
     }
 
--- src/execute/mod.rs
+++ src/execute/mod.rs
@@ -8,6 +8,31 @@ pub fn execute(expr: Expression) -> ExecuteResult<i64> {
     match expr {
         Expression::Number(n) => Ok(n),
         Expression::Add(left, right) => Ok(execute(*left)? + execute(*right)?),
-        _ => unimplemented!(),
+        Expression::Sub(left, right) => Ok(execute(*left)? - execute(*right)?),
+        Expression::Mult(left, right) => Ok(execute(*left)? * execute(*right)?),
+        Expression::Div(left, right) => {
+            let right_result = execute(*right)?;
+            if right_result != 0 {
+                Ok(execute(*left)? / right_result)
+            } else {
+                Err(ExecuteError::out_of_def("ゼロ除算エラー"))
+            }
+        },
+        Expression::Mod(left, right) => {
+            let right_result = execute(*right)?;
+            if right_result != 0 {
+                Ok(execute(*left)? % right_result)
+            } else {
+                Err(ExecuteError::out_of_def("ゼロ除算エラー"))
+            }
+        },
+        Expression::Pow(left, right) => {
+            let right_result = execute(*right)?;
+            if right_result >= 0 {
+                Ok(execute(*left)?.pow(right_result as u32))
+            } else {
+                Err(ExecuteError::out_of_def("指数が負です"))
+            }
+        },
     }
 }
